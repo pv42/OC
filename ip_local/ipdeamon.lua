760b74d2ec9744362c30af6cbd430080f5b8282b
pv42 <pv42.97@gmail.com>
2017-07-25 01:59:02 +0200

    arp arp arp arp

--- ip_local/ipdeamon.lua
+++ ip_local/ipdeamon.lua
@@ -1,1 +1,26 @@
 local libip = require("libip")
+local modem = require("modem")
+modem.open(libip.IP_PORT)
+modem.open(libip.ARP_PORT)
+while true do
+	_, _, from, port, _, msg = event.pull("modem_message")
+	if port == libip.IP_PORT then
+		if msg.version == 4 then
+			--...
+		end
+	elseif port == libip.ARP_PORT then
+		if message.hardware_adress_type == 1 and msg.protocol_adress_type == libip.IP_PORT then
+			libip.addToArpTable(msg.sorce_ip, msg.source_mac)
+			if msg.operation == libip.ARP_OP_REQ then
+				--if is request answer it
+				libip.sendArpPackage(libip.ARP_OP_ANSW, msg.source_mac, msg.source_ip)
+			end
+		else
+			--not matching
+			error("Invalid ARP request")
+		end
+	else
+		--unknown "ethernet" frame type
+		error("Invalid network frame type")
+	end 
+end
