c9f87c18d0457974b555710e31627e1e71c36499
pv42 <pv42.97@gmail.com>
2017-08-06 12:13:39 +0200

    added recive logic

--- ip_local/libtcp.lua
+++ ip_local/libtcp.lua
@@ -4,36 +4,76 @@ if libip== nil then error("no valid internet protocol active") end
 local TCP_DATA_OFFSET = 0
 local TCP_RESERVED = 0
 local TCP_WINDOW = 0
-local TCP_CHECKSUM = 12345
+local TCP_CHECKSUM = 35498
 local TCP_URGENT_POINTER = 0
 --locals
 local ports = {}
 
 
-function openConnection(sender_port_, target_adress_, target_port_)
-	conn = {packageBuffer={},sender_port = sender_port_, target_port = target_port_, target_adress = target_adress_ ,seq = random()}
-	lipip.sendIpPackage(target_adress, lipip.TOS_TCP, package)
+function openConnection(target_adress_, target_port_, local_port_)
+	if local_port_ == nil then local_port_ = get_free_port()
+	conn = {packageBuffer={},local_port = local_port_, remote_port = target_port_, remote_adress = target_adress_ ,seq = random(), ack = 0}
+	ports[target_port_] = conn -- marks port as used
+	conn.getNextSeq = function()
+		seq = seq + 1
+		return seq
+	end
+	ocflags = flags()
+	ocflags.SYN = true
+	sendTCPPackage(conn, nil, ocflags)
+	tcpp = reciveTCPPackage(conn)
+	if tcpp.flags.SYN and tcpp.flags.ACK then
+		sendTCPPackage(conn, nil, ack_flags(), tcpp.seq)
+		print("connection to " .. target_adress_ .. ":" .. target_port_ .. " opened")
+	else 
+		error("connection refused")
+	end
+end
+local function get_free_port()
+	port = 256
+	while ports[port] ~= nil do 
+		port = port + 1
+	end
+	return port
+end
+function awaitConnection(port)
+	-- body
 end
 function random()
 	return 19
 end
 function closeConnection(conn)
 	-- body
+	ports(conn.ta)
 end
-function reciveTCPPackage(conn)
-	-- body
+function reciveTCPPackage(conn, isSyn)
+	if isSyn then
+		while #conn.packageBuffer == 0 do
+			os.sleep(0.05)
+		end
+		for k,v in pairs(packageBuffer) do
+			conn.ack = k
+			return v
+		end
+	else
+		while(conn.packageBuffer[conn.ack + 1] == nil)
+			os.sleep(0.05)
+		end
+		return conn.packageBuffer[conn.ack + 1]
+	end
 end
-function sendTCPPackage( conn , data, flags)
-	if flags == nil then flags = send_flags() end
- 	local package = { source_port = conn.sender_port, destination_port = conn.target_port, seq = conn.getNextSeq(),
- 	 ack = 0, data_offset = TCP_DATA_OFFSET, reserved = TCP_RESERVED, flags = flags, window = TCP_WINDOW,
+function sendTCPPackage( conn , data, _flags, _ack)
+	if _flags == nil then _flags = flags() end
+	if _ack == nil then _ack = 0
+ 	local package = { source_port = conn.local_port, destination_port = conn.remote_port, seq = conn.getNextSeq(),
+ 	 ack = _ack, data_offset = TCP_DATA_OFFSET, reserved = TCP_RESERVED, flags = _flags, window = TCP_WINDOW,
  	 checksum = TCP_CHECKSUM, urget_pointer = TCP_URGENT_POINTER
 
 	}
- 	lipip.sendIpPackage(conn.target_adress, lipip.TOS_TCP, package)
+ 	lipip.sendIpPackage(conn.remote_adress, lipip.TOS_TCP, package)
 end
 
-local function send_flags()
+local function flags()
  	fl = {
  		ECE = false -- no collision
  		CWR = false -- no collision reaction
@@ -46,3 +86,9 @@ local function send_flags()
  	}
  	return fl
 end
+
+local function ack_flags()
+	fl = flags()
+	fl.ACK = true
+	return fl
+end = 1
