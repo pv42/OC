2d53e4a0f30f8732a93e2200ece40b61041fe27a
pv42 <pv42.97@gmail.com>
2019-07-06 13:52:21 +0200

    remove none-modem messages from nmon, shorten physical addresses in nmon

--- network/nmon.lua
+++ network/nmon.lua
@@ -17,26 +17,34 @@ pcall(function()
     else
       evt = table.pack(event.pull())
     end
-    if interactive then gpu.setForeground(0xCC2200) end
-    io.write("[" .. os.date("%T") .. "] ")
-    if interactive then gpu.setForeground(0x44CC00) end
     local type = tostring(evt[1]) 
-    io.write(type .. string.rep(" ", math.max(10 - #tostring(evt[1]), 0) + 1))
-    if interactive then gpu.setForeground(0xB0B00F) end
-    io.write(tostring(evt[2]) .. string.rep(" ", 37 - #tostring(evt[2])))
-    if interactive then gpu.setForeground(0xFFFFFF) end
-    if evt.n > 2 then
-      for i = 3, evt.n do
-        if type == "modem_message" and i == 4 then
-          if evt[i] == libip.IP_PORT then io.write("  IP") end
-          if evt[i] == libip.ARP_PORT then io.write("  ARP") end
-        else
-          io.write("  " .. tostring(evt[i]))
+    if type ~= "modem_message" then
+      if interactive then gpu.setForeground(0xCC2200) end
+      io.write("[" .. os.date("%T") .. "] ")
+      if interactive then gpu.setForeground(0x44CC00) end
+      io.write(type .. string.rep(" ", math.max(10 - #tostring(evt[1]), 0) + 1))
+      if interactive then gpu.setForeground(0xB0B00F) end
+      io.write(string.sub(tostring(evt[2]),1,8) .. " "))
+      if interactive then gpu.setForeground(0xFFFFFF) end
+      if evt.n > 2 then
+        for i = 3, evt.n do
+          if i == 4 then
+            if evt[i] == libip.IP_PORT then 
+              io.write("  IP") 
+            elseif evt[i] == libip.ARP_PORT then 
+              io.write("  ARP")
+            else 
+              io.write("  " .. tostring(evt[i])) 
+            end
+          elseif i == 3 then
+            io.write("  " .. string.sub(tostring(evt[i]),1,8))
+          else  
+            io.write("  " .. tostring(evt[i]))
+          end
         end
       end
+      io.write("\n")
     end
-   
-    io.write("\n")
   until evt[1] == "interrupted"
 end)
 if interactive then
