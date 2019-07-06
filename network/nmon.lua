5dd77687e9903bdd22db6303057d6d0d3af362bb
pv42 <pv42.97@gmail.com>
2019-07-06 14:01:16 +0200

    remove print of local phy address and event type from nmon

--- network/nmon.lua
+++ network/nmon.lua
@@ -9,6 +9,17 @@ local color, isPal, evt
 if interactive then
   color, isPal = gpu.getForeground()
 end
+
+local function frame_type_to_str(frame_type)
+  if frame_type == libip.IP_PORT then 
+    return "IP" 
+  elseif frame_type == libip.ARP_PORT then 
+    return "ARP"
+  else 
+    return tostring(frame_type) 
+  end
+end
+
 io.write("Press 'Ctrl-C' to exit\n")
 pcall(function()
   repeat
@@ -18,25 +29,19 @@ pcall(function()
       evt = table.pack(event.pull())
     end
     local type = tostring(evt[1]) 
-    if type ~= "modem_message" then
+    if type == "modem_message" then
+      local frame_type = evt[4]
+
       if interactive then gpu.setForeground(0xCC2200) end
       io.write("[" .. os.date("%T") .. "] ")
       if interactive then gpu.setForeground(0x44CC00) end
-      io.write(type .. string.rep(" ", math.max(10 - #tostring(evt[1]), 0) + 1))
+      io.write(frame_type_to_str(frame_type) .. string.rep(" ", math.max(6 - #tostring(frame_type_to_str(frame_type)), 0) + 1))
       if interactive then gpu.setForeground(0xB0B00F) end
-      io.write(string.sub(tostring(evt[2]),1,8) .. " "))
+      io.write(string.sub(tostring(evt[3]),1,8) .. " ")
       if interactive then gpu.setForeground(0xFFFFFF) end
       if evt.n > 2 then
-        for i = 3, evt.n do
-          if i == 4 then
-            if evt[i] == libip.IP_PORT then 
-              io.write("  IP") 
-            elseif evt[i] == libip.ARP_PORT then 
-              io.write("  ARP")
-            else 
-              io.write("  " .. tostring(evt[i])) 
-            end
-          elseif i == 3 then
+        for i = 5, evt.n do
+          if i == 3 then
             io.write("  " .. string.sub(tostring(evt[i]),1,8))
           else  
             io.write("  " .. tostring(evt[i]))
