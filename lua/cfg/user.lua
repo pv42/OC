c366eef3830d4ef7a139aa5bb7dc5220cd713b8c
pv42 <pv42.97@gmail.com>
2019-08-01 00:12:26 +0200

    removed lua and venv

--- lua/cfg/user.lua
+++
@@ -1,8 +0,0 @@
---[[--
-  Use this file to specify **System** preferences.
-  Review [examples](+C:\Users\pv42\Downloads\ZeroBraneStudio\cfg\user-sample.lua) or check [online documentation](http://studio.zerobrane.com/documentation.html) for details.
---]]--
-styles = loadfile('cfg/tomorrow.lua')('TomorrowNightEighties')
-stylesoutshell = styles -- apply the same scheme to Output/Console windows
-styles.auxwindow = styles.text -- apply text colors to auxiliary windows
-styles.calltip = styles.text -- apply text colors to tooltips
--- venv/component.lua
+++
@@ -1,6 +0,0 @@
-local component = {}
-component.gpu = {}
-component.gpu.fill = print
-component.gpu.set = print
-
-return component
