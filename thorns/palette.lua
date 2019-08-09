6d7039389e705d54bffca2d4ace8072681fdfd59
pv42 <pv42.97@gmail.com>
2019-08-09 02:58:27 +0200

    fix palette program

--- thorns/palette.lua
+++ thorns/palette.lua
@@ -1,10 +1,11 @@
+local gpu = require("component").gpu
 for i = 0, 15 do
   local color = gpu.getPaletteColor(i)
   gpu.setBackground(color)
-  if color > 0x7ffff then
+  if color > 0x7fffff then
     gpu.setForeground(0x0)
   else
     gpu.setForeground(0xffffff)
   end
-  print(string.format("%02d: 0x%x", color))
+  print(string.format("%02d: 0x%x", i, color))
 end
