1fdd6b258a8a6bd249486f14a730bc6667634cbd
pv42 <pv42.97@gmail.com>
2019-08-16 23:23:54 +0200

    implement random, more interactive rce

--- network/libtcp.lua
+++ network/libtcp.lua
@@ -1,13 +1,17 @@
 -- libtcp by pv42
--- TODO allow multible connections to one port
 
---libs
+--imports
 local log = require("log")
-local libip = require("libip") 
--- local serialization = require("serialization")
+local libip = require("libip")
+local serialization = require("serialization")
+local math = require("math")
+
 log.i("loading tcp libary")
+if libip == nil then
+  error("no valid internet protocol active")
+end
 libtcp = {}
-if libip== nil then error("no valid internet protocol active") end
+
 -- const
 local TCP_DATA_OFFSET = 0
 local TCP_RESERVED = 0
@@ -23,21 +27,20 @@ local C_SYN_RCV = 2
 local C_SYN_SENT = 3
 local C_ESTABLISHED = 4
 --
-local TOS_TCP = 6 
+local TOS_TCP = 6
 --locals
 local ports = {}
 
-
 local function get_free_port()
   port = 512
-  while ports[port] ~= nil do 
+  while ports[port] ~= nil do
     port = port + 1
   end
   return port
 end
 
 local function random()
-  return 19 -- wtf, not even a fair dice roll
+  return math.floor(math.random() * 65535)
 end
 
 --flags
@@ -65,7 +68,7 @@ local function fin_flags(ack)
   fl = flags(ack)
   fl.FIN = true
   return fl
-end 
+end
 
 --class connection
 
@@ -74,13 +77,17 @@ libtcp.Connection = {}
 libtcp.Connection.__index = libtcp.Connection
 
 function libtcp.Connection:open(target_address, target_port, local_port)
-  if local_port == nil then local_port = get_free_port() end
-  if ports[port] ~= nil then error("port is already used") end
+  if local_port == nil then
+    local_port = get_free_port()
+  end
+  if ports[port] ~= nil then
+    error("port is already used")
+  end
   ports[local_port] = conn -- marks port as used
-  local conn = { packageBuffer_r={}, packageBuffer_s={},
+  local conn = { packageBuffer_r = {}, packageBuffer_s = {},
                  local_port = local_port, remote_port = target_port,
-                 remote_address = target_address, seq = random(), ack = 0, state = C_CLOSED}
-  setmetatable(conn,libtcp.Connection)
+                 remote_address = target_address, seq = random(), ack = 0, state = C_CLOSED }
+  setmetatable(conn, libtcp.Connection)
   -- syn
   conn:send(nil, syn_flags())
   conn.state = C_SYN_SENT
@@ -93,23 +100,28 @@ function libtcp.Connection:open(target_address, target_port, local_port)
     conn:send(nil, flags(true))
     conn.state = C_ESTABLISHED
     print("connection to " .. target_address .. ":" .. target_port .. " opened")
-  else 
+  else
     error("connection refused")
   end
-    table.insert(connections, conn)
-    return conn
+  table.insert(connections, conn)
+  return conn
 end
 
-
 function libtcp.Connection:listen(port)
-  if ports[port] ~= nil then error("port is already used") end
-  local conn = {packageBuffer_r={}, packageBuffer_s={}, packageSendTimeStep = {},
-    local_port = port, remote_port =nil,remote_adress = nil, -- not set yet
-    seq = random(), ack = 0}
-  setmetatable(conn,libtcp.Connection)
+  if ports[port] ~= nil then
+    error("port is already used")
+  end
+  local conn = { packageBuffer_r = {}, packageBuffer_s = {}, packageSendTimeStep = {},
+                 local_port = port, remote_port = nil, remote_adress = nil, -- not set yet
+                 seq = random(), ack = 0 }
+  setmetatable(conn, libtcp.Connection)
   ports[port] = conn
-  conn:m_receivePackage(10,true)
+  p = conn:m_receivePackage(0, true)
+  if not p then
+    error("could not establish connection")
+  end
   conn:send(nil, syn_flags(true))
+  print("TCP:established")
   return conn
 end
 
@@ -128,9 +140,11 @@ function libtcp.Connection:m_receivePackage(timeout, isSyn)
     while #self.packageBuffer_r == 0 do
       os.sleep(0.05)
       i = i + 1
-      if i >= timeout * 20 then return end -- timeout
+      if i >= timeout * 20 then
+        return
+      end -- timeout
     end
-    for k,v in pairs(packageBuffer) do
+    for k, v in pairs(packageBuffer) do
       self.ack = k
       return v
     end
@@ -143,13 +157,17 @@ function libtcp.Connection:m_receivePackage(timeout, isSyn)
 end
 
 function libtcp.Connection:send(data, _flags)
-  if _flags == nil then _flags = flags() end
-  if _ack == nil then _ack = 0 end
+  if _flags == nil then
+    _flags = flags()
+  end
+  if _ack == nil then
+    _ack = 0
+  end
   local package = { source_port = self.local_port, destination_port = self.remote_port, seq = self:getNextSeq(),
-   ack = _ack, data_offset = TCP_DATA_OFFSET, reserved = TCP_RESERVED, flags = _flags, window = TCP_WINDOW,
-   checksum = TCP_CHECKSUM, urget_pointer = TCP_URGENT_POINTER, data = data
+                    ack = _ack, data_offset = TCP_DATA_OFFSET, reserved = TCP_RESERVED, flags = _flags, window = TCP_WINDOW,
+                    checksum = TCP_CHECKSUM, urget_pointer = TCP_URGENT_POINTER, data = data
   }
-  self.packageBuffer_s[package.seq] = {package=package, send_try=0, time=os.time()}
+  self.packageBuffer_s[package.seq] = { package = package, send_try = 0, time = os.time() }
 end
 
 function libtcp.Connection:close()
@@ -163,36 +181,37 @@ end
 -- end class
 
 function libtcp.handleTCPPackeage(tcpp, senderIP)
-  if ports[tcpp.destination_port] == nil then error("recived tcpp on closed port " .. tcpp.destination_port) 
-	else
-		conn = ports[tcpp.destination_port]
-		if tcpp.flags.ACK then
-			conn.packageBuffer_s[tcpp.ack] = nil -- package acknowleged, must not be send again
-		end
+  if ports[tcpp.destination_port] == nil then
+    error("recived tcpp on closed port " .. tcpp.destination_port)
+  else
+    conn = ports[tcpp.destination_port]
+    if tcpp.flags.ACK then
+      conn.packageBuffer_s[tcpp.ack] = nil -- package acknowleged, must not be send again
+    end
     if not tcpp.flags.ACK or tcpp.flags.SYN then
-			conn.packageBuffer_r[tcpp.seq] = tcpp -- put in rec buffer and acknoledge
-			lib.sendTCPPackage(conn, nil, ack_flags(), tcpp.seq)
-		end
-	end
+      conn.packageBuffer_r[tcpp.seq] = tcpp -- put in rec buffer and acknoledge
+      lib.sendTCPPackage(conn, nil, ack_flags(), tcpp.seq)
+    end
+  end
 end
 
 --called by the network deamon
-function libtcp.sendStep() 
-	for _, conn in pairs(connections) do 
-		if conn.state ~= C_CLOSED then
+function libtcp.sendStep()
+  for _, conn in pairs(connections) do
+    if conn.state ~= C_CLOSED then
       for seq, data in pairs(conn.packageBuffer_s) do
-			  if data.time -  os.time() > TCP_ACK_TIMEOUT then 	
-			 	  if data.send_try >= TCP_MAX_SEND_TRIES then
+        if data.time - os.time() > TCP_ACK_TIMEOUT then
+          if data.send_try >= TCP_MAX_SEND_TRIES then
             conn.state = C_CLOSED -- too many timeouts
             log.e("connection closed due too many timeouts")
           else
             lipip.sendIpPackage(conn.remote_adress, TOS_TCP, data.package)
             data.send_try = data.send_try + 1 -- might not work
           end
-			  end	
-			end
-		end
-	end
-end 
+        end
+      end
+    end
+  end
+end
 
 return libtcp
--- rce/rceclient.lua
+++ rce/rceclient.lua
@@ -5,10 +5,13 @@ print("rce-client") -- rci stands for remote code execution
 print("enter remote ip")
 local ip_str = io.read()
 local ip = libip.StringtoIP(ip_str)
-local conn = tcp.Connection:open(ip,PORT)
-while true do
-  print("connected")
-  local cmd = io.read()
-  conn:send(cmd)
-  print(conn:receive())
-end
+repeat
+  local suc, conn = pcall(tcp.Connection.open,nil,ip,PORT) -- tcp.Connection:open(ip,PORT)
+  if not suc then print("Connection could not be established. retrying ..") end
+  while suc do
+    print("connected")
+    local cmd = io.read()
+    conn:send(cmd)
+    print(conn:receive())
+  end
+until(suc)
